c5d2b411017909d20a178282d83b6edae7bdeca4
Sametkuscu <116021210+Sametkuscu@users.noreply.github.com>
2022-10-24 19:02:14 +0300

    Create Taksimetre2.java

---
+++ Taksimetre2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Taksimetre2 {
+    public static void main(String[] args) {
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Gideceğiniz yer kaç km?: ");
+        int km = girdi.nextInt();
+        int startPrice = 10;
+        double perKm = 2.20;
+        double toplam = (km*perKm) + startPrice;
+        double toplam2 = (toplam<20)? 20: toplam;
+        System.out.print(toplam2);
+    }
+
+}
